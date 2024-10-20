fa0413a97aa635c42b138aa2c22d651d973a5970
Erick Gualpa <m.erickgualpa@gmail.com>
2024-10-21 00:15:33 +0200

    Remove aggregate_id tag from wallet_domain_events_published metric

--- src/main/kotlin/org/egualpam/contexts/observable/walletinteractionservice/wallet/adapters/metrics/DomainEventMetrics.kt
+++ src/main/kotlin/org/egualpam/contexts/observable/walletinteractionservice/wallet/adapters/metrics/DomainEventMetrics.kt
@@ -1,6 +1,7 @@
 package org.egualpam.contexts.observable.walletinteractionservice.wallet.adapters.metrics
 
 import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.DistributionSummary
 import io.micrometer.core.instrument.MeterRegistry
 import org.egualpam.contexts.observable.walletinteractionservice.shared.application.domain.DomainEvent
 
@@ -9,7 +10,6 @@ class DomainEventMetrics(
 ) {
   fun published(domainEvent: DomainEvent) {
     Counter.builder("wallet_domain_events_published")
-        .tag("aggregate_id", domainEvent.aggregateId().value())
         .tag("type", domainEvent.javaClass.simpleName)
         .register(meterRegistry)
         .increment()
