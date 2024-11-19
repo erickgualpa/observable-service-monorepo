837a5e894212848b7d21c1a098557a67fb8cc17b
Erick Gualpa <m.erickgualpa@gmail.com>
2024-11-19 19:45:47 +0100

    Add dummy async deposits endpoint

---
+++ src/main/kotlin/org/egualpam/contexts/observable/walletinteractionservice/wallet/adapters/in/controllers/PutWalletDepositControllerV2.kt
@@ -0,0 +1,67 @@
+package org.egualpam.contexts.observable.walletinteractionservice.wallet.adapters.`in`.controllers
+
+import org.egualpam.contexts.observable.walletinteractionservice.shared.application.domain.exceptions.InvalidAggregateId
+import org.egualpam.contexts.observable.walletinteractionservice.shared.application.domain.exceptions.InvalidDomainEntityId
+import org.egualpam.contexts.observable.walletinteractionservice.wallet.adapters.metrics.ErrorMetrics
+import org.egualpam.contexts.observable.walletinteractionservice.wallet.application.domain.exceptions.AccountCurrencyIsNotSupported
+import org.egualpam.contexts.observable.walletinteractionservice.wallet.application.usecases.command.DepositMoney
+import org.egualpam.contexts.observable.walletinteractionservice.wallet.application.usecases.command.DepositMoneyCommand
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.accepted
+import org.springframework.http.ResponseEntity.badRequest
+import org.springframework.http.ResponseEntity.internalServerError
+import org.springframework.transaction.support.TransactionTemplate
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture.runAsync
+
+@RequestMapping("/v2/wallets")
+@RestController
+class PutWalletDepositControllerV2(
+  private val transactionTemplate: TransactionTemplate,
+  private val depositMoney: DepositMoney,
+  private val errorMetrics: ErrorMetrics,
+) {
+  private val logger: Logger = getLogger(this::class.java)
+
+  @PutMapping("/{wallet-id}/deposit")
+  fun putWalletDeposit(
+    @PathVariable("wallet-id") walletId: String,
+    @RequestBody putWalletDepositRequest: PutWalletDepositRequest
+  ): ResponseEntity<Void> {
+    val depositMoneyCommand = DepositMoneyCommand(
+        walletId,
+        depositId = putWalletDepositRequest.deposit.id,
+        depositAmount = putWalletDepositRequest.deposit.amount,
+        depositCurrency = putWalletDepositRequest.deposit.currency,
+    )
+    return try {
+      runAsync {
+        transactionTemplate.executeWithoutResult {
+          depositMoney.execute(depositMoneyCommand)
+        }
+      }
+      accepted().build()
+    } catch (e: RuntimeException) {
+      errorMetrics.error(e)
+      when (e.javaClass) {
+        InvalidAggregateId::class.java,
+        InvalidDomainEntityId::class.java,
+        AccountCurrencyIsNotSupported::class.java -> {
+          logger.warn(e.message)
+          badRequest().build()
+        }
+
+        else -> {
+          logger.error("Unexpected error processing request [$putWalletDepositRequest]:", e)
+          internalServerError().build()
+        }
+      }
+    }
+  }
+}
